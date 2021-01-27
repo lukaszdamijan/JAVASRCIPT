0af0221ac9a48663cc6fbb23580b71078a0ffe9b
lukaszdamijan <72335024+lukaszdamijan@users.noreply.github.com>
2021-01-27 21:04:23 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,16 @@
+//Zadanie 1
+
+console.log("Łukasz");
+
+//Zadanie 2
+
+console.log(12+32);
+console.log(52/16);
+console.log(5*2);
+
+//Zadanie 3
+
+console.log("Hello World");
+console.log(Hello World); 
+console.log("Hello World); 
+// tylko piewrsza linia jest poprawna, przy pozostałych pojawia się błąd.

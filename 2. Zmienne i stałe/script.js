a7fbd6b4679d9156138438cbc7e1f9c3f37f293b
lukaszdamijan <72335024+lukaszdamijan@users.noreply.github.com>
2021-01-27 21:06:25 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,28 @@
+// zadanie 1
+
+let number1 = 2;
+let text1 = "text";
+let numberAndString = 2 + "dwa";
+let logic = 3 < 2;
+let special = null;
+
+console.log(number1);
+console.log(text1);
+console.log(numberAndString);
+console.log(logic);
+console.log(special);
+
+// zadanie 2
+
+var numberOne = 5;
+var numberTwo = 3;
+var sum = 0;
+sum = numberOne + numberTwo;
+console.log(sum)
+
+// zadanie 3
+
+let test;
+console.log(test);
+
+// zmienna  undefined ponieważ nie ma wartości

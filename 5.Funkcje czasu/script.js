ccef7a680340e76b39261aa1dbc2e450a6dd423d
lukaszdamijan <72335024+lukaszdamijan@users.noreply.github.com>
2021-01-27 21:11:55 +0100

    Update script.js

---
+++ 5.Funkcje czasu/script.js
@@ -0,0 +1,27 @@
+//Zadanie 1
+
+var x = 0;
+var n = setInterval(() => {
+    if(x == 15)
+        {
+            clearInterval(n);
+        }
+       console.log("Cześć po raz "+ x++)
+}, 1000);
+
+//Zadanie 2
+
+let tab = ["Maciek", "Jakub", "Aleksandra"]; 
+setTimeout(function() {
+for (let i=0; i<tab.length; i++) {
+    console.log(tab[i]);
+}
+i = 0;
+setInterval(function() {
+  
+    if(tab.length>i) {
+    console.log(tab[i++]);
+}
+}, 3000);
+  
+}, 2000);

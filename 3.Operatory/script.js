d441ac3b733d3fbda35c2b3896cfdf4fe63ab7e7
lukaszdamijan <72335024+lukaszdamijan@users.noreply.github.com>
2021-01-27 21:07:45 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,50 @@
+// zadanie 1
+
+let bool1 = ture;
+let bool2 = false;
+
+console.log(bool1 === bool2);
+
+//Zadanie 2
+
+var number1 = 2;
+var number2 = 5;
+var moduloResult = 0;
+moduloResult = number2 % number1;
+console.log(moduloResult);
+
+//Zadanie 3
+
+let word1 = "Pies";
+let word2 = "Kot";
+
+let stringResult = "";
+
+stringResult = word1 + word2;
+
+console.log(stringResult);
+
+//Zadanie 4:
+
+var someNumber =425;
+var someString ="425";
+
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+
+//Zadanie 5:
+
+var counter = 30;
+console.log(counter); //30
+counter++;
+console.log(counter); //31
+counter--;
+console.log(counter); //30
+
+//Zadanie 6:
+
+let somevar1 = 1;
+let somevar2 = 2;
+let result = null;
+result = somevar1 > somevar2;
+console.log(result);
